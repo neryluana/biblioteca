b7211d58bad8e5f7bef475a41aa0d9294c197ac7
LUANA NERY DA SILVA LIMA <luana.nslima1@senacsp.edu.br>
2023-07-31 17:28:23 -0300

    Gerando a migration

--- biblioteca/Data/BibliotecaContext.cs
+++ biblioteca/Data/BibliotecaContext.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Models;
 
-namespace biblioteca.Data
+namespace Biblioteca.Data
 {
-    public class BibliotecaContext: DbContext
+    public class BibliotecaContext : DbContext
     {
-        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options) 
-        { 
-        
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options)
+            : base(options)
+        {
         }
+
+        public DbSet<Book> Book { get; set; } = default!;
+        public DbSet <Publisher> Publisher { get; set; } = default!;
+
+        public DbSet<Author> Author { get; set; } = default!;
+
+
 
     }
 }
---
+++ biblioteca/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace Biblioteca.Models
+{
+    [Table("Books")]
+    public class Book
+    {
+        // criando entidade
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        // colocando um nome na tabela
+        [Column("Book_Name")]
+        public string? Title { get; set; }
+        public string? ISBN { get; set; }
+        public Publisher? Publisher { get; set; }
+
+
+
+        public ICollection<Author>? Authors { get; set; }
+
+
+
+
+
+    }
+}
